91609da7c84d20616bcf14180f9a7061f6690ebd
TaliaEspejo <taliamariamela18@gmail.com>
2025-04-30 04:08:47 -0300

    actualizacion del archivo por Espejo Talia Selene

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,53 @@
-// App.jsx
-import React, { useState } from 'react';             
-import TaskInput from './components/TaskInput';      
-import TaskList from './components/TaskList';   
-import './App.css'     
-const App = () => {
-  const [tasks, setTasks] = useState([]);            
-  const [inputValue, setInputValue] = useState('');  
+//app
+import React, { useState } from 'react';
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
+import './App.css';
 
-  const handleAddTask = () => {                      
-    if (inputValue.trim() === '') return;
+const App = () => {
+  const [tasks, setTasks] = useState([]); 
+  const [inputValue, setInputValue] = useState(''); 
+
+  // Función para agregar una tarea
+  const handleAddTask = (text) => {
+    if (text.trim() === '') return; // Evita agregar tareas vacías
 
     const newTask = {
-      id: Date.now(),
-      text: inputValue,
-      completed: false,
+      id: Date.now(),   
+      text,             
+      completed: false, 
     };
 
     setTasks([...tasks, newTask]);
-    setInputValue('');
+    setInputValue(''); 
   };
 
-  const handleToggleCompleted = (id) => {            //marcar como completada la tarea
+  // Función para alternar el estado de completado de una tarea
+  const handleToggleCompleted = (id) => {
     const updatedTasks = tasks.map((task) =>
       task.id === id ? { ...task, completed: !task.completed } : task
     );
     setTasks(updatedTasks);
   };
 
-  const handleDeleteTask = (id) => {                 //elimina tareas
+  // Función para eliminar una tarea
+  const handleDeleteTask = (id) => {
     const filteredTasks = tasks.filter((task) => task.id !== id);
     setTasks(filteredTasks);
   };
 
   return (
     <div>
-      <h1>Listado de Tareas</h1>
-      <TaskInput                                    // entrada 
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        onAddTask={handleAddTask}
+      <h1>Listado de las Tareas</h1>
+      <TaskInput
+        inputValue={inputValue}  
+        setInputValue={setInputValue}  
+        onAddTask={handleAddTask} 
       />
-      <TaskList                                     // muestra
-        tasks={tasks}
-        onToggleCompleted={handleToggleCompleted}
-        onDelete={handleDeleteTask}
+      <TaskList
+        tasks={tasks}  
+        onToggleCompleted={handleToggleCompleted}  
+        onDelete={handleDeleteTask}  
       />
     </div>
   );
