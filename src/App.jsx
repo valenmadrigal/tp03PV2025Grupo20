079c61ac271f8f21f26a997c84c7ada9c6f42f31
Franco <francoarcibia423@gmail.com>
2025-05-10 21:18:30 -0300

    Mejoramento de productos "Franco Arcibia"

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,22 @@
-// App.jsx
-import React, { useState } from 'react';             
-import TaskInput from './components/TaskInput';      
-import TaskList from './components/TaskList';   
-import './App.css'     
-const App = () => {
-  const [tasks, setTasks] = useState([]);            
-  const [inputValue, setInputValue] = useState('');  
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Producto from './assets/component/Productos.jsx'
 
-  const handleAddTask = () => {                      
-    if (inputValue.trim() === '') return;
 
-    const newTask = {
-      id: Date.now(),
-      text: inputValue,
-      completed: false,
-    };
-
-    setTasks([...tasks, newTask]);
-    setInputValue('');
-  };
-
-  const handleToggleCompleted = (id) => {            //marcar como completada la tarea
-    const updatedTasks = tasks.map((task) =>
-      task.id === id ? { ...task, completed: !task.completed } : task
-    );
-    setTasks(updatedTasks);
-  };
-
-  const handleDeleteTask = (id) => {                 //elimina tareas
-    const filteredTasks = tasks.filter((task) => task.id !== id);
-    setTasks(filteredTasks);
-  };
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Listado de Tareas</h1>
-      <TaskInput                                    // entrada 
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        onAddTask={handleAddTask}
-      />
-      <TaskList                                     // muestra
-        tasks={tasks}
-        onToggleCompleted={handleToggleCompleted}
-        onDelete={handleDeleteTask}
-      />
-    </div>
+   <div className="contenedor-principal" style={{ 
+  textAlign: 'center', 
+  textDecoration: 'underline', 
+  fontWeight: 'bold' 
+}}>
+  <h1>Programacion Visual - TP3</h1>
+  <Producto></Producto>
+</div>
   );
-};
+}
 
-export default App;
+export default App
